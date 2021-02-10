2990cea3a20ae67ae0d320b86f36cf2fa07de8b8
Rioja <lulyrioja@gmail.com>
2021-02-09 23:44:46 -0300

    :alien: Creating functions to populate the table

--- script.js
+++ script.js
@@ -30,3 +30,53 @@ const Transaction = {
   expenses() {},
   total() {},
 };
+
+const DOM = {
+  transactionsContainer: document.querySelector("#data-table tbody"),
+
+  addTransaction(transaction, index) {
+    const tr = document.createElement("tr");
+    tr.innerHTML = DOM.innerHTMLTransaction(transaction);
+
+    DOM.transactionsContainer.appendChild(tr);
+  },
+
+  innerHTMLTransaction(transaction) {
+    const CSSclass = transaction.amount > 0 ? "income" : "expense";
+    const amount = Utils.formatCurrency(transaction.amount);
+
+    const html = `
+      <td class="description">${transaction.description}</td>
+      <td class="${CSSclass}">${amount}</td>
+      <td class="date">${transaction.date}</td>
+      <td>
+        <img src="./assets/minus.svg" alt="Remover transação" />
+      </td>
+    `;
+
+    return html;
+  },
+};
+
+const Utils = {
+  formatCurrency(value) {
+    const signal = Number(value) < 0 ? "-" : "";
+
+    // Na expressão regular o \D é tudo que não é número.
+    // O g depois da expressão regular é para indicar que a busca será global.
+    value = String(value).replace(/\D/g, "");
+
+    value = Number(value) / 100;
+
+    value = value.toLocaleString("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    });
+
+    return signal + value;
+  },
+};
+
+transactions.forEach((transaction) => {
+  DOM.addTransaction(transaction);
+});
